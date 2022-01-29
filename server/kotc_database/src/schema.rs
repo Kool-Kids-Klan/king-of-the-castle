cde017a1178f5567ea1ac517c36b817cf662e2fb
492988 <492988@mail.muni.cz>
2022-01-29 20:45:39 +0100

    fmt changes

--- server/kotc_database/src/schema.rs
+++ server/kotc_database/src/schema.rs
@@ -31,4 +31,8 @@ joinable!(games -> users (winner_id));
 joinable!(participations -> games (game_id));
 joinable!(participations -> users (user_id));
 
-allow_tables_to_appear_in_same_query!(games, participations, users,);
+allow_tables_to_appear_in_same_query!(
+    games,
+    participations,
+    users,
+);
