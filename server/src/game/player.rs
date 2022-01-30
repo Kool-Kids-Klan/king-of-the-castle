e87a754b4d1d08bcc29a674212377b659c2dbcec
Daniel Timko <492777@mail.muni.cz>
2022-01-30 22:35:43 +0100

    [server] WIP: Drawing and playing cards

--- server/src/game/player.rs
+++ server/src/game/player.rs
@@ -1,13 +1,13 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 
-use super::card::Card;
+use super::card::{Card, Character};
 use super::User;
-use crate::game::Character;
 
 #[derive(Clone, Debug)]
 pub struct Player<'a> {
-    user: &'a User,
+    pub user: &'a User,
+    hand: Vec<Card>,
     deck: Vec<Card>,
     score: u8,
 }
@@ -16,14 +16,22 @@ impl Player<'_> {
     pub fn new(user: &User) -> Player {
         let mut player = Player {
             user,
+            hand: vec![],
             deck: vec![],
             score: 0,
         };
         player.refill_deck();
+        player.draw_card();
+        player.draw_card();
+        player.draw_card();
         player
     }
 
     fn refill_deck(&mut self) {
+        let except: Vec<Character> = self.hand.clone()
+            .into_iter()
+            .map(|card| card.character)
+            .collect();
         let initial_deck = vec![
             Card::new(self.user.username.clone(), Character::Kral, 20.0),
             Card::new(self.user.username.clone(), Character::Kralovna, 16.0),
@@ -50,9 +58,24 @@ impl Player<'_> {
             Card::new(self.user.username.clone(), Character::Drak, 11.0),
             Card::new(self.user.username.clone(), Character::Romeo, 5.0),
             Card::new(self.user.username.clone(), Character::Zebrak, 4.0),
-        ];
+        ].into_iter().filter(|card| !except.contains(&card.character)).collect();
         let mut rng = thread_rng();
         self.deck = initial_deck;
         self.deck.shuffle(&mut rng);
+    }
+
+    fn next_card(&mut self) -> Card {
+        match self.deck.pop() {
+            Some(card) => card,
+            None => {
+                self.refill_deck();
+                self.next_card()
+            }
+        }
+    }
+
+    pub fn draw_card(&mut self) {
+        let card = self.next_card();
+        self.hand.push(card);
     }
 }
