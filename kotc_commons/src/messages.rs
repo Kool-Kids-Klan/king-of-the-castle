d07200d79ab9d023797eb6e5394adfcdabab984c
492988 <492988@mail.muni.cz>
2022-02-02 23:52:55 +0100

    create common messages

--- kotc_commons/src/messages.rs
+++ kotc_commons/src/messages.rs
@@ -1,24 +1,51 @@
+pub mod message_types;
+
 use serde::{Deserialize, Serialize};
-use actix::prelude::Message;
 
-use kotc_game::game::card::Card;
-use kotc_game::game::column::Column;
+use crate::messages::message_types::{ClientWsMessageType, ServerWsMessageType};
 
-#[derive(Serialize, Deserialize, Message)]
-#[rtype(result = "()")]
-pub struct UpdateBoard {
-    pub board: Vec<Column>,
+/// ****CLIENT MESSAGES****
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ClientWsMessage {
+    pub message_type: ClientWsMessageType,
+    pub content: String,
 }
 
-#[derive(Serialize, Deserialize, Message)]
-#[rtype(result = "()")]
-pub struct UpdateHand {
-    pub hand: Vec<Card>,
-}
-
-#[derive(Serialize, Deserialize, Message)]
-#[rtype(result = "()")]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct PlayCard {
     pub card_index: usize,
     pub column_index: usize
 }
+
+/// ****SERVER MESSAGES****
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ServerWsMessage {
+    pub message_type: ServerWsMessageType,
+    pub content: String,
+}
+
+//
+// #[derive(Serialize, Deserialize, Message, Debug)]
+// #[rtype(result = "()")]
+// pub struct ServerWsMessage<T: ServerWsMessages> {
+//     pub message_type: ServerWsMessageType,
+//     pub content: T,
+// }
+//
+// #[derive(Serialize, Deserialize, Message, Debug)]
+// #[rtype(result = "()")]
+// pub struct UpdateBoard {
+//     pub board: Vec<Column>,
+// }
+//
+// impl ServerWsMessages for UpdateBoard {}
+//
+// #[derive(Serialize, Deserialize, Message, Debug)]
+// #[rtype(result = "()")]
+// pub struct UpdateHand {
+//     pub hand: Vec<Card>,
+// }
+//
+// impl ServerWsMessages for UpdateHand {}
