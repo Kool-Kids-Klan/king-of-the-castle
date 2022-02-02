807e6b04fc9f158bd1495e7a49bb14737adc956b
Daniel Timko <492777@mail.muni.cz>
2022-02-02 16:41:37 +0100

    Move game logic to separate Cargo package

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,3 @@
-extern crate core;
-
-mod game;
-
 use kotc_actix::start_actix_server;
 
 #[actix_web::main]
@@ -9,44 +5,4 @@ async fn main() -> anyhow::Result<()> {
     start_actix_server().await?;
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::game::{card::{Card, Character}, Token, column::Column, Resource};
-
-    #[test]
-    fn test_column_eval() {
-        let cards = vec![
-            Card::new(String::from("ccc"), Character::Doppelganger, 0.0),
-            Card::new(String::from("bbb"), Character::Doppelganger, 0.0),
-            Card::new(String::from("aaa"), Character::King, 20.0),
-            Card::new(String::from("aaa"), Character::Queen, 16.0),
-            Card::new(String::from("aaa"), Character::Julia, 14.0),
-            Card::new(String::from("aaa"), Character::Alchemist, 8.0),
-            Card::new(String::from("aaa"), Character::Swordsman, 8.0),
-            Card::new(String::from("bbb"), Character::Landlord, 8.0),
-            Card::new(String::from("bbb"), Character::Merchant, 8.0),
-            Card::new(String::from("aaa"), Character::Cardinal, 8.0),
-            Card::new(String::from("bbb"), Character::Troubadour, 8.0),
-            Card::new(String::from("aaa"), Character::Explorer, 13.0),
-            Card::new(String::from("aaa"), Character::Killer, 9.5),
-            Card::new(String::from("aaa"), Character::Storm, 9.0),
-            // Card::new(String::from("ccc"), Character::Prevlek, 0.0),
-            // Card::new(String::from("aaa"), Character::Zradca, 10.0),
-            // Card::new(String::from("aaa"), Character::Musketyri, 11.0),
-            // Card::new(String::from("ccc"), Character::Mag, 7.0),
-            // Card::new(String::from("aaa"), Character::Carodejnice, 1.0),
-            // Card::new(String::from("bbb"), Character::Princ, 14.0),
-            Card::new(String::from("bbb"), Character::Squire, 2.0),
-            Card::new(String::from("aaa"), Character::Hermit, 12.0),
-            Card::new(String::from("ccc"), Character::Thumb, 2.0),
-            Card::new(String::from("aaa"), Character::Dragon, 11.0),
-            Card::new(String::from("aaa"), Character::Romeo, 5.0),
-            // Card::new(String::from("aaa"), Character::Zebrak, 4.0),
-        ];
-        let mut column = Column::new(Token {resource: Resource::Coins, points: 3});
-        cards.into_iter().for_each(|card| column.add_card(card));
-        println!("AND THE WINNER IS: {}", column.eval());
-    }
 }
