e732b7d749a82d2d25773944ec1ff09d82f94c75
Pavol Baran <pbaran@redhat.com>
2022-02-02 15:41:10 +0100

    [client] fixed html indentation

--- client/src/pages/register.rs
+++ client/src/pages/register.rs
@@ -3,16 +3,16 @@ use yew::prelude::*;
 #[function_component(Register)]
 pub fn register() -> Html {
     return html! {
-    <div class="register" >
-        <form class="form">
-            <label class="form__label" for="username">{"Username:"}</label>
-            <input class="form__input" type="text" id="username" name="username"/>
-            <label class="form__label" for="email">{"Email:"}</label>
-            <input class="form__input" type="text" id="email" name="email"/>
-            <label class="form__label" for="password">{"Password:"}</label>
-            <input class="form__input" type="Password" id="password" name="password" />
-            <button class="form__button" type="submit">{"Register"}</button>
-        </form>
-    </div>
+        <div class="register" >
+            <form class="form">
+                <label class="form__label" for="username">{"Username:"}</label>
+                <input class="form__input" type="text" id="username" name="username"/>
+                <label class="form__label" for="email">{"Email:"}</label>
+                <input class="form__input" type="text" id="email" name="email"/>
+                <label class="form__label" for="password">{"Password:"}</label>
+                <input class="form__input" type="Password" id="password" name="password" />
+                <button class="form__button" type="submit">{"Register"}</button>
+            </form>
+        </div>
    }
 }
