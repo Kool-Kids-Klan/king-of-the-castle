c20382dade6681d381196652232eb43c6e53a18c
Pavol Baran <pbaran@redhat.com>
2022-02-02 15:41:10 +0100

    [client] fixed html indentation

--- client/src/pages/game.rs
+++ client/src/pages/game.rs
@@ -3,8 +3,8 @@ use yew::prelude::*;
 #[function_component(Game)]
 pub fn game() -> Html {
     return html! {
-    <div class="game">
-       <h1>{"Game"}</h1>
-    </div>
+        <div class="game">
+           <h1>{"Game"}</h1>
+        </div>
    }
 }
