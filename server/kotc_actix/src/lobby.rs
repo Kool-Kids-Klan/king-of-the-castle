c2e4261cf203eb7a1c168dcb1d37a86ac47e4e5f
492988 <492988@mail.muni.cz>
2022-02-06 15:31:15 +0100

    fix lobby and game

--- server/kotc_actix/src/lobby.rs
+++ server/kotc_actix/src/lobby.rs
@@ -1,16 +1,19 @@
+use std::cell::{Ref, RefCell};
 use kotc_game::game::Game;
 use std::collections::HashSet;
+use std::ops::Deref;
+use std::rc::Rc;
 
 #[derive(Clone)]
 pub struct Lobby {
-    pub game: Game,
+    pub game: Rc<RefCell<Game>>,
     pub sessions: HashSet<usize>,
 }
 
 impl Lobby {
     pub fn new() -> Self {
         Lobby {
-            game: Game::new(),
+            game: Rc::new(RefCell::new(Game::new())),
             sessions: HashSet::new(),
         }
     }
